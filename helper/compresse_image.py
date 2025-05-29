325b8c64aa44ff3fd77d04a810bb939c5d4d2c77
nut <nut999anan@hotmail.com>
2025-05-29 09:52:39 +0700

    update dol compressed image and fodler structure

--- helper/compresse_image.py
+++ helper/compresse_image.py
@@ -2,15 +2,17 @@ from PIL import Image
 import os
 Image.MAX_IMAGE_PIXELS = None
 
-# Set the folder paths
-input_folder = '../dol'  # Folder where original images are stored
-output_folder = '../images'  # Folder where compressed images will be stored
+input_folder = '../dol_images'  
+output_folder = '../dol_compressed'  
 
-# Create the output folder if it doesn't exist
+input_folder = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '..','static' ,'dol_images')
+output_folder = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '..', 'static','dol_compressed')
+
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Function to resize the image while maintaining aspect ratio
 
 
 def resize_image(img, max_dimension=3000):
@@ -18,7 +20,6 @@ def resize_image(img, max_dimension=3000):
     max_side = max(width, height)
 
     if max_side > max_dimension:
-        # Calculate the scale ratio to resize the image
         scale_factor = max_dimension / max_side
         new_width = int(width * scale_factor)
         new_height = int(height * scale_factor)
@@ -26,27 +27,20 @@ def resize_image(img, max_dimension=3000):
 
     return img
 
-# Function to compress images
 
 
 def compress_image(image_path, output_path, max_size_mb=10):
     try:
-        # Open an image file
         Image.MAX_IMAGE_PIXELS = None
         with Image.open(image_path) as img:
-            # Resize the image if necessary
             img = resize_image(img)
-            # Convert image to RGB (removes alpha if present)
             img = img.convert('RGB')
 
-            # Save the image with quality control
             quality = 85
             while True:
                 img.save(output_path, quality=quality)
-                # Check file size
                 if os.path.getsize(output_path) <= max_size_mb * 1024 * 1024:
                     break
-                # Reduce quality if file size is too large
                 quality -= 5
 
             print(
@@ -54,13 +48,11 @@ def compress_image(image_path, output_path, max_size_mb=10):
     except Exception as e:
         print(f"Error processing {image_path}: {e}")
 
-
-# Loop through all files in the input folder
+image_count = 0
 for filename in os.listdir(input_folder):
     if filename.endswith(('.png', '.jpg', '.jpeg')):
-        # Full path to the image
+        image_count += 1
         image_path = os.path.join(input_folder, filename)
-        # Set the output path for compressed image
         output_path = os.path.join(output_folder, filename)
-        # Compress the image
         compress_image(image_path, output_path)
+
---
+++ ocr/paid_ocr/openai_ocr.py
@@ -0,0 +1,73 @@
+import openai
+import dotenv
+import os
+import base64
+from openai import OpenAI
+
+
+# Load environment variables from .env file
+dotenv.load_dotenv()
+# Initialize OpenAI API Key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+# Path to your image
+# image_path = os.path.join(os.path.dirname(os.path.abspath(
+#     __file__)), '..', 'images', '03-11.jpg')
+
+
+input_folder = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '..', 'images')
+output_folder = os.path.join(os.path.dirname(os.path.abspath(
+    __file__)), '..', 'research_results','ocr_results', 'openai_ocr_results')
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+
+
+
+def openai_ocr_image_to_text(image_path):
+    # Getting the Base64 string
+    base64_image = encode_image(image_path)
+
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    { "type": "input_text", "text": "This Image is landdeeds document in thai could you extract the text from this image" },
+                    {
+                        "type": "input_image",
+                        "image_url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                ],
+            }
+        ],
+    )
+
+    return response.output_text
+
+
+
+for image_file in os.listdir(input_folder):
+    # Check if the file is an image (you can add more extensions if needed)
+    if image_file.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image_path = os.path.join(input_folder, image_file)
+        
+        # Process the image and get the extracted text
+        extracted_text = openai_ocr_image_to_text(image_path)
+
+        # Save the extracted text to a text file
+        text_file_path = os.path.join(output_folder, f"{os.path.splitext(image_file)[0]}.txt")
+        
+        with open(text_file_path, 'w', encoding='utf-8') as f:
+            f.write(extracted_text)
+
+        print(f"Text from {image_file} saved to {text_file_path}")
